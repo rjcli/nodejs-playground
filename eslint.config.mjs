8cb34337b19db8b83a9d0296e8c409fcc8389483
Rashmi Raj <rashmiraj7877@gmail.com>
2025-03-24 03:01:33 +0530

    Add rules for eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,7 +3,28 @@ import pluginJs from '@eslint/js';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  { files: ['**/*.js'], languageOptions: { sourceType: 'commonjs' } },
-  { languageOptions: { globals: globals.browser } },
+  {
+    files: ['**/*.js'],
+    languageOptions: { sourceType: 'commonjs' },
+    rules: {
+      'spaced-comment': 'error',
+      'no-console': 'off',
+      'consistent-return': 'off',
+      'func-names': 'off',
+      'object-shorthand': 'off',
+      'no-process-exit': 'off',
+      'no-param-reassign': 'error',
+      'no-return-await': 'off',
+      'no-underscore-dangle': 'off',
+      'class-methods-use-this': 'off',
+      'prefer-destructuring': ['error', { object: true, array: false }],
+      'no-unused-vars': ['error', { argsIgnorePattern: 'req|res|next|val' }],
+      eqeqeq: 'error',
+      indent: ['error', 2],
+    },
+  },
+  {
+    languageOptions: { globals: globals.browser },
+  },
   pluginJs.configs.recommended,
 ];
